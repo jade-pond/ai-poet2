6a750c0dad90bb4b1872ceaaa8c5df43d1093471
jade-pond <167834779+jade-pond@users.noreply.github.com>
2024-08-05 20:02:16 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from langchain_community.chat_models import ChatOllama
+llm = ChatOllama(model="EEVE-Korean-10.8B:latest")
+
+
+import streamlit as st
+
+st.title("인공지능 제이드")
+st.title("만나서 반가워요. 제이드에요. :sunglasses:")
+
+content = st.text_input("여기에 주제를 입력해주세요:")
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('제이드 생각 중...'):
+        result = llm.invoke(content + "에 대한 시를 써줘") 
+        st.write(result)
+
+
